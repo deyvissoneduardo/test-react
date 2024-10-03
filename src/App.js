6e1d8e968985a2b3549045221ae1b271d7cf5751
Deyvisson Eduardo <deyvissoneduardo22@gmail.com>
2024-10-03 19:29:57 -0300

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <div className="text-container">
+        <p className="animated-text">
+          Oi, você é incrível pra caramba, sabia? Mas olha, não precisa de ninguém que te complete,
+          porque você já é completa. O que você merece é alguém que te transborde, que te ajude a ser
+          ainda mais você. Sério, olha esse sorriso, esse teu jeito único... e, ah, esse corpo... 😍😍
+          Se você pudesse se enxergar com os meus olhos, veria o quanto já é extraordinária.
+          <br /><br />
+          E é exatamente por isso que a menina do "eu consigo sozinha" merece o cara do "eu sei, mas
+          deixa eu te ajudar". Não pra te completar, mas pra caminhar junto, lado a lado, te apoiando em tudo.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
